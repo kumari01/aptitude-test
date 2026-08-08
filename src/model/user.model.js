25496f70f659c0d511964481d397a9d794eac5e0
HemanthTammisetty <hemanth.tammisetty@sasi.ac.in>
2026-08-08 09:27:51 +0530

    Updated auth controller, user model, and router

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -20,11 +20,27 @@ const studentSchema = new mongoose.Schema({
         type: String,
         required:true
     },
-    role:{
-        type:String,
-        enum:["student","admin"],
-        default:"student"
-    }
 })
-
+const adminSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required:true,
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true
+    },
+     adminid:{
+        type: String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type: String,
+        required:true
+    }
+   
+})
 module.exports = mongoose.model('Student',studentSchema);
+module.exports = mongoose.model('Admin',adminSchema);
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const authController = require('../Controllers/auth.controller');
 
-router.post('/signup', authController.RegisterStudent);
-router.post('/login', authController.studentlogin);
+router.post('/student/signup', authController.RegisterStudent);
+router.post('/student/login', authController.studentlogin);
+router.post('/admin/signup', authController.adminregister);
+router.post('/admin/login', authController.adminlogin);
 module.exports = router;
